303cdfd7e9974210640831fb39c9d8cd4089e90c
Nakalinzisylvia <syldiana7154@gmail.com>
2025-07-04 16:38:26 +0300

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@
 
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css">
   <title>dashboard</title>
 </head>
 <nav class="navbar navbar-light bg-light">
@@ -20,3 +20,109 @@
     </form>
   </div>
 </nav>
+
+<body>
+  <div class="container-xl">
+    <div class="row">
+      <div class="col-lg-3">
+        <div class="card mb-5">
+          <div class="card-body">
+            <h5 class="card-title">DEV Community is a community of 3,292,028 amazing developers </h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">We're a place where coders share, stay up-to-date and grow their careers. </p>
+            <a href="#" class="card-link">login </a>
+            <a href="#" class="card-link"> create account</a>
+          </div>
+        </div>
+
+        <div>
+
+          <p class="fs-0.5"> <span><i class="bi bi-0-square-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                </svg></i></span>Home</p>
+          <p class="fs-0.5"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+              </svg></span>Dev++</p>
+          <p class="fs-0.5"><span></span>Podcasts</p>
+          <p class="fs-0.5"><span></span>Videos</p>
+          <p class="fs-0.5"><span></span>Tags</p>
+          <p class="fs-0.5"><span></span>Dev Education Tracks</p>
+          <p class="fs-0.5"><span></span>Dev Challenges</p>
+          <p class="fs-0.5"><span></span>Dev Challenges</p>
+          <p class="fs-0.5"><span></span>Advertise on DEV</p>
+          <p class="fs-0.5"><span></span>DEV Showcase</p>
+          <p class="fs-0.5"><span></span>About</p>
+          <p class="fs-0.5"><span></span>Contact</p>
+          <p class="fs-0.5"><span></span>Free Postgres Database</p>
+          <p class="fs-0.5"><span></span>Software Comparisons</p>
+          <p class="fs-0.5"><span></span>Forem Shop</p>
+
+
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <button type="button" class="btn btn-light">Relevant</button>
+        <button type="button" class="btn btn-light">Latest</button>
+        <button type="button" class="btn btn-light">Top</button>
+        <div class="card mb-3">
+          <img src="https://media2.dev.to/dynamic/image/width=775%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fh7klyxm3yd1cdq47vko5.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title"> Introducing a new prompt: Holistic Webdev!</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+          <img src="https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fdwdmtjlpqvn67do5p8oa.png" class="card-img-bottom" alt="...">
+        </div>
+      </div>
+
+      <div class="col-lg-3">
+        <div class="card">
+          <img src="https://media2.dev.to/dynamic/image/width=880%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fg691oplv5qw5gijyxfr5.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">title </h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="https://media2.dev.to/dynamic/image/width=880%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fh7klyxm3yd1cdq47vko5.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="https://media2.dev.to/dynamic/image/width=880%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fsqgw4o18woyt919bnz6l.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
